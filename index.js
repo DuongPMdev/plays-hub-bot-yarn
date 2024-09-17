5b1987cc4925e97e419fb3ee1c1f0b2c874691ac
DESKTOP-46EGB0G\NGen PC <duongpm13dev@gmail.com>
2024-09-17 15:57:06 +0700

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+
+import TelegramBot from 'node-telegram-bot-api'
+import config from 'config'
+
+const TOKEN = config.get('token')
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+bot.onText(/\/start/, (msg, [source, match]) => {
+	const {chat: {id, username}} = msg
+	const photoUrl = "https://catb.io/game/banner.png"
+	const captionDes = `Welcome to Cat Battle Game!\n\nDive into a captivating storyline and experience deep gameplay that will keep you hooked for hours everyday.\n\nBOOST CAT POWER TO EARN!`
+	bot.sendPhoto(id, photoUrl, {
+		caption: captionDes,
+		reply_markup: {
+			inline_keyboard: [
+				[
+					{
+						text: '🤜🤛 Play Game',
+						web_app:{
+							url: "https://catb.io/game",
+						} 
+					}
+				]
+			]
+		}
+	})
+})
